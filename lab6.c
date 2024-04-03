c53058ae4f7204cbbdc2bfc776d3036faacd1596
DragosStezar <dstezar@outlook.com>
2024-04-03 17:06:35 +0300

    Lab7 - Comparare snapshot uri pentru un singur director

--- lab6.c
+++ lab6.c
@@ -8,19 +8,64 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
-#define SIZE 1000
+#define SIZE 1024
 
 struct stat stat_buffer;
 
-void salvare_snaphot(int fd , char *cale_director)
+void clonare_snaphot(char sn1[] , int fd1 , char sn2[] , int fd2)
 {
-    if(lstat(cale_director , &stat_buffer)==0)
+    lseek(fd1 , 0 , SEEK_SET);
+    lseek(fd2 , 0 , SEEK_SET);
+    char buf2[SIZE];
+    int count1=0;
+    while((count1=read(fd2 , buf2 , SIZE))>0)
     {
-        if(S_ISREG(stat_buffer.st_mode))
+        write(fd1 , buf2 , count1);
+    }
+}
+
+int comparare_snapshot(char sn1[] , int fd1 , char sn2[] , int fd2)
+{
+    fstat(fd1 , &stat_buffer);
+    int size1=stat_buffer.st_size;
+    fstat(fd2 , &stat_buffer);
+    int size2=stat_buffer.st_size;
+    if(size1!=size2)
+    {
+        return 1;
+    }
+    else
+    {
+        char buf1[SIZE];
+        char buf2[SIZE];
+        int count1;
+        int count2;
+        lseek(fd1 , 0 , SEEK_SET);
+        lseek(fd2 , 0 , SEEK_SET);
+        while((count1=read(fd1 , buf1 , SIZE))>0 && (count2=read(fd2 , buf2 , SIZE))>0)
         {
-            dprintf(fd , "%s %ld %s\n" , cale_director , stat_buffer.st_size , ctime(&stat_buffer.st_mtime));
+            ///printf("%s %s" , buf1 , buf2);
+            if(strlen(buf1)!=strlen(buf2))
+            {
+                return 1;
+            }
+            for(int i=0 ; i<strlen(buf1) ; i++)
+            {
+                if(buf1[i]!=buf2[i])
+                {
+                    return 1;
+                }
+            }
         }
     }
+    return 0;
+}
+
+void salvare_snaphot(int fd , char *cale_director)
+{
+    char aux[SIZE]="";
+    
+    dprintf(fd , "%s %ld %s\n" , cale_director , stat_buffer.st_size , ctime(&stat_buffer.st_mtime));
 }
 
 void creare_snaphot(int fd , char *cale_director)
@@ -71,15 +116,47 @@ int main(int argc , char **argv)
         perror("Nu este director");
         exit(0);
     }
-    int fd=open("snapshot.txt" , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-    if(fd==-1)
+    char nume_dir1[SIZE]="";
+    strcat(nume_dir1 , "snapshot_");
+    strcat(nume_dir1 , argv[1]);
+    strcat(nume_dir1 , "_1.txt");
+
+    int fd1=open(nume_dir1 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+    if(fd1==-1)
     {
         perror("Eroare fisier");
         exit(-1);
     }
+    if(lstat(nume_dir1 , &stat_buffer)==0)
+    {
+        if(stat_buffer.st_size == 0)
+        {
+            creare_snaphot(fd1 , argv[1]);
+        }
+        
+        char nume_dir2[SIZE]="";
+        strcat(nume_dir2 , "snapshot_");
+        strcat(nume_dir2 , argv[1]);
+        strcat(nume_dir2 , "_2.txt");
 
-    creare_snaphot(fd , argv[1]);
+        int fd2=open(nume_dir2 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+        if(fd2==-1)
+        {
+            perror("Eroare fisier");
+            exit(-1);
+        }
 
-    close(fd);
+        creare_snaphot(fd2 , argv[1]);
+        if(comparare_snapshot(nume_dir1 , fd1 , nume_dir2 , fd2)==1)
+        {
+            clonare_snaphot(nume_dir1 , fd1 , nume_dir2 , fd2);
+            unlink(nume_dir2);
+        }
+        else
+        {
+            unlink(nume_dir2);
+        }
+    }
+    close(fd1);
     return 0;
 }
