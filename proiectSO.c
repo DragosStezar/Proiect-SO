67a0f18c6bd8c9e282d182a9f907446bf222dac1
DragosStezar <dstezar@outlook.com>
2024-04-04 18:44:55 +0300

    Lab7 - H cerinta 2 salvare snapshoturi in director output

--- proiectSO.c
+++ proiectSO.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <ctype.h>
 #define SIZE 1024
 
 struct stat stat_buffer;
@@ -65,7 +66,8 @@ void salvare_snaphot(int fd , char *cale_director)
 {
     char aux[SIZE]="";
     
-    dprintf(fd , "%s %ld %s\n" , cale_director , stat_buffer.st_size , ctime(&stat_buffer.st_mtime));
+    int length_buf=snprintf(aux , SIZE , "%s %ld %s\n" , cale_director , stat_buffer.st_size , ctime(&stat_buffer.st_mtime));
+    write(fd , aux , length_buf);
 }
 
 void creare_snaphot(int fd , char *cale_director)
@@ -101,62 +103,71 @@ void creare_snaphot(int fd , char *cale_director)
 
 int main(int argc , char **argv)
 {
-    if(argc != 2)
+    if(argc > 12)
     {
         perror("Eroare argumente");
         exit(0);
     }
-    if(lstat(argv[1] , &stat_buffer)!=0)
-    {
-        perror("Eroare director");
-        exit(0);
-    }
-    if(S_ISDIR(stat_buffer.st_mode)==0)
-    {
-        perror("Nu este director");
-        exit(0);
-    }
-    char nume_dir1[SIZE]="";
-    strcat(nume_dir1 , "snapshot_");
-    strcat(nume_dir1 , argv[1]);
-    strcat(nume_dir1 , "_1.txt");
 
-    int fd1=open(nume_dir1 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-    if(fd1==-1)
+    char dir_out[SIZE];
+    strcpy(dir_out , argv[2]);   
+
+    for(int i=3 ; i<argc ; i++)
     {
-        perror("Eroare fisier");
-        exit(-1);
-    }
-    if(lstat(nume_dir1 , &stat_buffer)==0)
-    {
-        if(stat_buffer.st_size == 0)
+        if(lstat(argv[i] , &stat_buffer)!=0)
         {
-            creare_snaphot(fd1 , argv[1]);
+            perror("Eroare director");
+            exit(0);
         }
-        
-        char nume_dir2[SIZE]="";
-        strcat(nume_dir2 , "snapshot_");
-        strcat(nume_dir2 , argv[1]);
-        strcat(nume_dir2 , "_2.txt");
+        if(S_ISDIR(stat_buffer.st_mode)==0)
+        {
+            perror("Nu este director");
+            exit(0);
+        }
+        char nume_dir1[SIZE]="";
+        strcat(nume_dir1 , dir_out);
+        strcat(nume_dir1 , "/snapshot_");
+        strcat(nume_dir1 , argv[i]);
+        strcat(nume_dir1 , "_1.txt");
 
-        int fd2=open(nume_dir2 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-        if(fd2==-1)
+        int fd1=open(nume_dir1 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+        if(fd1==-1)
         {
             perror("Eroare fisier");
             exit(-1);
         }
+        if(lstat(nume_dir1 , &stat_buffer)==0)
+        {
+            if(stat_buffer.st_size == 0)
+            {
+                creare_snaphot(fd1 , argv[i]);
+            }
+            
+            char nume_dir2[SIZE]="";
+            strcat(nume_dir2 , dir_out);
+            strcat(nume_dir2 , "/snapshot_");
+            strcat(nume_dir2 , argv[i]);
+            strcat(nume_dir2 , "_2.txt");
 
-        creare_snaphot(fd2 , argv[1]);
-        if(comparare_snapshot(nume_dir1 , fd1 , nume_dir2 , fd2)==1)
-        {
-            clonare_snaphot(nume_dir1 , fd1 , nume_dir2 , fd2);
-            unlink(nume_dir2);
+            int fd2=open(nume_dir2 , O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+            if(fd2==-1)
+            {
+                perror("Eroare fisier");
+                exit(-1);
+            }
+
+            creare_snaphot(fd2 , argv[i]);
+            if(comparare_snapshot(nume_dir1 , fd1 , nume_dir2 , fd2)==1)
+            {
+                clonare_snaphot(nume_dir1 , fd1 , nume_dir2 , fd2);
+                unlink(nume_dir2);
+            }
+            else
+            {
+                unlink(nume_dir2);
+            }
         }
-        else
-        {
-            unlink(nume_dir2);
-        }
+        close(fd1);
     }
-    close(fd1);
     return 0;
 }
